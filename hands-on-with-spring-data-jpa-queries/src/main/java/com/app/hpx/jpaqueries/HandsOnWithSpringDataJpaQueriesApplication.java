2c1e68f7ecc0f614a5de272a9b33bec31f285330
HPxIO <hpx.io@keemail.me>
2022-05-01 13:19:30 +0530

    init-project

---
+++ hands-on-with-spring-data-jpa-queries/src/main/java/com/app/hpx/jpaqueries/HandsOnWithSpringDataJpaQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.app.hpx.jpaqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnWithSpringDataJpaQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnWithSpringDataJpaQueriesApplication.class, args);
+	}
+
+}
---
+++ hands-on-with-spring-data-jpa-queries/src/test/java/com/app/hpx/jpaqueries/HandsOnWithSpringDataJpaQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hpx.jpaqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnWithSpringDataJpaQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
